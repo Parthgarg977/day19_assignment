150b12ce2604669ff71a28c28ae2f714b54b3f77
parthgarg977 <parthgarg977@gmail.com>
2022-09-11 07:00:51 +0530

    2nd commit

--- day19_assignment/Program.cs
+++ day19_assignment/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to user registration program");
+Console.WriteLine("Select Options Please :\n" +"1-Enter First Name\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user = new UserRegistration();
+                    user.FirstName();
+                    break;
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
---
+++ day19_assignment/UserRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+    public class UserRegistration
+    {
+        public void FirstName()
+        {
+            Console.WriteLine("Please Enter First Name :");
+            string data = Console.ReadLine();
+
+            string pattern = "^[A-Z][a-z]{2,10}$";
+
+            Regex regobj = new Regex(pattern);
+
+            if (regobj.IsMatch(data))
+            {
+                Console.WriteLine("First Name is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+                Console.ResetColor();
+            }
+        }
+    }
